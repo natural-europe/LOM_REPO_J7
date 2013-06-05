833b52116810c5a46a5eea324474bc7abaf81f58
vogias <vogias@admin.grnet.gr>
2013-06-05 14:35:24 +0300

    c

--- src/org/ariadne_eu/metadata/resultsformat/ResultDelegateARIADNERFJS.java
+++ src/org/ariadne_eu/metadata/resultsformat/ResultDelegateARIADNERFJS.java
@@ -28,7 +28,8 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
-	private static Logger log = Logger.getLogger(ResultDelegateARIADNERFJS.class);
+	private static Logger log = Logger
+			.getLogger(ResultDelegateARIADNERFJS.class);
 
 	private int start;
 	private int max;
@@ -39,13 +40,19 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 		try {
 			facetFields = new Vector<String>();
 
-			Collection solrs = PropertiesManager.getInstance().getPropertyStartingWith(RepositoryConstants.getInstance().SR_SOLR_FACETFIELD + ".").values();
+			Collection solrs = PropertiesManager
+					.getInstance()
+					.getPropertyStartingWith(
+							RepositoryConstants.getInstance().SR_SOLR_FACETFIELD
+									+ ".").values();
 			for (Object object : solrs) {
 				facetFields.add((String) object);
 			}
 
 			if (!(facetFields.size() > 0)) {
-				log.error("initialize:property \"" + RepositoryConstants.getInstance().SR_SOLR_FACETFIELD + ".n\" not defined");
+				log.error("initialize:property \""
+						+ RepositoryConstants.getInstance().SR_SOLR_FACETFIELD
+						+ ".n\" not defined");
 			}
 
 		} catch (Throwable t) {
@@ -59,11 +66,12 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 		this.lQuery = lQuery;
 	}
 
-	public String result(TopDocs topDocs, IndexSearcher searcher) throws JSONException, CorruptIndexException, IOException {
+	public String result(TopDocs topDocs, IndexSearcher searcher)
+			throws JSONException, CorruptIndexException, IOException {
 		SolrDocument doc;
 
 		QueryResponse response = getSolrResponse();
-		
+
 		JSONObject resultsJson = new JSONObject();
 		JSONObject resultJson = new JSONObject();
 		JSONArray idArrayJson = new JSONArray();
@@ -71,24 +79,29 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 		resultJson.put("error", "");
 		resultJson.put("errorMessage", "");
 		resultJson.put("facets", getFacets(response.getFacetFields()));
-		
-		
-		int size = (int)response.getResults().getNumFound();
+
+		int size = (int) response.getResults().getNumFound();
 		if (size == -1)
 			size = Integer.MAX_VALUE;
-		
-		for (int i = 0; i < max && (max < 0 || i < size - start) ; i++) {
+
+		for (int i = 0; i < max && (max < 0 || i < size - start); i++) {
 			JSONObject json = new JSONObject();
 			doc = response.getResults().get(i);
 			try {
 				idArrayJson.put(doc.get("lom.general.identifier.entry"));
-				
-				addJsonObjectWE(doc, json, "lom.general.title.langstring", "title");
-				addJsonObjectWE(doc, json, "lom.general.title.string", "title");
-				addJsonObjectWE(doc, json, "lom.general.identifier.entry", "identifier");
 
-				addJsonObjectWE(doc, json, "lom.general.description.langstring",
+				addJsonObjectWE(doc, json, "lom.general.title.langstring",
+						"title");
+				addJsonObjectWE(doc, json, "lom.general.title.string", "title");
+
+				addJsonObjectWE(doc, json, "lom.general.identifier.entry",
+						"identifier");
+
+				addJsonObjectWE(doc, json,
+						"lom.general.description.langstring", "descriptions");
+				addJsonObjectWE(doc, json, "lom.general.description.string",
 						"descriptions");
+
 				addJsonObjectWE(doc, json, "lom.general.language", "language");
 
 				addJsonObjectWE(doc, json, "lom.general.keyword.langstring",
@@ -104,9 +117,8 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 				addJsonObjectWE(doc, json, "lom.educational.context.value",
 						"context");
 
-				addJsonObjectWE(doc, json, "lom.technical.size",
-						"size");
-				
+				addJsonObjectWE(doc, json, "lom.technical.size", "size");
+
 				addJsonObjectWE(doc, json, "lom.metametadata.identifier.entry",
 						"metaMetadataId");
 
@@ -119,7 +131,7 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 				addJsonObjectWE(doc, json,
 						"lom.rights.copyrightandotherestrictions.value",
 						"license");
-//
+				//
 				addJsonObjectWE(doc, json,
 						"lom.educational.learningresourcetype.value", "type");
 				addJsonObject(doc, json, "lom.lifecycle.contribute.role.value",
@@ -145,15 +157,17 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 
 				addJsonObjectWE(doc, json, "lom.annotation.date.datetime",
 						"annotation date");
+
 				addJsonObjectWE(doc, json, "lom.rights.description.langstring",
 						"rights");
 				addJsonObjectWE(doc, json, "lom.rights.description.string",
 						"rights");
+
 				addJsonObjectWE(doc, json, "lom.classification.purpose.value",
 						"classification purpose");
-				addJsonObjectWE(doc, json, "lom.relation.resource.identifier.entry",
-						"relation");
-			
+				addJsonObjectWE(doc, json,
+						"lom.relation.resource.identifier.entry", "relation");
+
 				addJsonObject(doc, json, "mdPath", "mdPath");
 
 			} catch (JSONException ex) {
@@ -166,9 +180,10 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 		resultJson.put("nrOfResults", size);
 
 		resultsJson.put("result", resultJson);
-		
+
 		return resultsJson.toString();
 	}
+
 	private void addJsonObjectWE(SolrDocument doc, JSONObject json,
 			String fieldName, String responeseName) throws JSONException {
 
@@ -185,6 +200,11 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 
 		Collection<Object> fieldValues = doc.getFieldValues(fieldName);
 		Collection<Object> fieldLangValues = doc.getFieldValues(langAttributes);
+
+		if (fieldLangValues == null) {
+			langAttributes = fieldName + ".xml:lang";
+			fieldLangValues = doc.getFieldValues(langAttributes);
+		}
 
 		if (fieldValues != null && fieldLangValues != null) {
 
@@ -257,26 +277,31 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 		// else
 		// json.put(responeseName, new String(""));
 	}
+
 	private QueryResponse getSolrResponse() {
 		SolrServerManagement serverMgt = SolrServerManagement.getInstance();
 
-		SolrQuery solrQuery = new SolrQuery().setQuery(lQuery).setFacet(true).setFacetLimit(-1).setFacetMinCount(1).setFacetSort(FacetParams.FACET_SORT_COUNT).setParam("rows", Integer.toString(max)).setParam("start", Integer.toString(start));
+		SolrQuery solrQuery = new SolrQuery().setQuery(lQuery).setFacet(true)
+				.setFacetLimit(-1).setFacetMinCount(1)
+				.setFacetSort(FacetParams.FACET_SORT_COUNT)
+				.setParam("rows", Integer.toString(max))
+				.setParam("start", Integer.toString(start));
 
-
-		for (Iterator<String> iterator = facetFields.iterator(); iterator.hasNext();) {
+		for (Iterator<String> iterator = facetFields.iterator(); iterator
+				.hasNext();) {
 			String facetField = (String) iterator.next();
 			solrQuery.addFacetField(facetField);
 		}
 		QueryResponse rsp = null;
-		
+
 		try {
 			rsp = serverMgt.getServer().query(solrQuery);
-			
+
 		} catch (SolrServerException e) {
 			log.error("getSolrResponse: Solr server error", e);
 		} catch (IOException e) {
 			log.error("getSolrResponse: Solr I/O error", e);
-		} 
+		}
 		return rsp;
 	}
 
@@ -287,17 +312,21 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 				List<Count> facetValues;
 				FacetField facetField;
 				FacetField.Count innerFacetField;
-				for (Iterator facetIterator = facetsFields.iterator(); facetIterator.hasNext();) {
+				for (Iterator facetIterator = facetsFields.iterator(); facetIterator
+						.hasNext();) {
 					JSONObject facetJson = new JSONObject();
 					facetField = (FacetField) facetIterator.next();
-					facetJson.put("field", changeFacetName(facetField.getName()));
+					facetJson.put("field",
+							changeFacetName(facetField.getName()));
 
 					facetValues = facetField.getValues();
 					if (facetValues != null) {
 						JSONArray valuesJson = new JSONArray();
-						for (Iterator ifacetIterator = facetValues.iterator(); ifacetIterator.hasNext();) {
+						for (Iterator ifacetIterator = facetValues.iterator(); ifacetIterator
+								.hasNext();) {
 							JSONObject value = new JSONObject();
-							innerFacetField = (FacetField.Count) ifacetIterator.next();
+							innerFacetField = (FacetField.Count) ifacetIterator
+									.next();
 							value.put("val", innerFacetField.getName());
 							value.put("count", innerFacetField.getCount());
 							valuesJson.put(value);
@@ -315,7 +344,8 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 	}
 
 	private String changeFacetName(String internalName) {
-		if (internalName.equalsIgnoreCase("lom.educational.learningresourcetype.value"))
+		if (internalName
+				.equalsIgnoreCase("lom.educational.learningresourcetype.value"))
 			return "lrt";
 		else if (internalName.equalsIgnoreCase("lom.educational.context.value"))
 			return "context";
@@ -325,29 +355,37 @@ public class ResultDelegateARIADNERFJS implements IndexSearchDelegate {
 			return "language";
 		else if (internalName.equalsIgnoreCase("collection"))
 			return "provider";
-		else if (internalName.equalsIgnoreCase("lom.educational.interactivitytype.value"))
+		else if (internalName
+				.equalsIgnoreCase("lom.educational.interactivitytype.value"))
 			return "it";
-		else if (internalName.equalsIgnoreCase("lom.educational.interactivitylevel.value"))
+		else if (internalName
+				.equalsIgnoreCase("lom.educational.interactivitylevel.value"))
 			return "il";
-		else if (internalName.equalsIgnoreCase("lom.educational.intendedenduserrole.value"))
+		else if (internalName
+				.equalsIgnoreCase("lom.educational.intendedenduserrole.value"))
 			return "iur";
-		else if (internalName.equalsIgnoreCase("lom.educational.typicalagerange.string"))
+		else if (internalName
+				.equalsIgnoreCase("lom.educational.typicalagerange.string"))
 			return "tagr";
 		else if (internalName.equalsIgnoreCase("lom.general.keyword.string"))
 			return "keyword";
 		else if (internalName.equalsIgnoreCase("lom.rights.description.string"))
 			return "rights";
-		else if (internalName.equalsIgnoreCase("lom.rights.copyrightandotherrestrictions.string"))
+		else if (internalName
+				.equalsIgnoreCase("lom.rights.copyrightandotherrestrictions.string"))
 			return "licences";
-                 else if (internalName.equalsIgnoreCase("lom.classification.taxonpath.taxon.entry.langstring"))
+		else if (internalName
+				.equalsIgnoreCase("lom.classification.taxonpath.taxon.entry.langstring"))
 			return "classification";
-                else if (internalName.equalsIgnoreCase("lom.educational.typicalagerange.string"))
+		else if (internalName
+				.equalsIgnoreCase("lom.educational.typicalagerange.string"))
 			return "temporal";
-                else if (internalName.equalsIgnoreCase("lom.general.coverage.string"))
+		else if (internalName.equalsIgnoreCase("lom.general.coverage.string"))
 			return "spatial";
-                else if (internalName.equalsIgnoreCase("lom.classification.description.string"))
+		else if (internalName
+				.equalsIgnoreCase("lom.classification.description.string"))
 			return "common";
 
-			return internalName;
+		return internalName;
 	}
 }
